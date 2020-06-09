cd2ae91d20b3f01efe81c25bd3e4dd5dd7f2e9c4
sachin <sachin.ee@iitb.ac.in>
2020-06-09 10:16:08 +0530

    first commit

---
+++ dia/asr_diarize.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+diarization_type=$1
+audio_path=$2
+input_file=$3
+output_dir=$4
+. ./conf/sad.conf
+. ./path.sh
+. ./cmd.sh
+nj=1
+test_sets=dev_test
+stage=9
+sad_stage=0
+use_new_rttm_reference=false
+score=false
+diarizer_stage=0
+#######################################################################
+# Perform DATA Prepration
+#######################################################################
+
+if [ $stage -le 1 ];then
+mkdir -p data.bak
+cp -r data/* data.bak/
+rm -rf data
+
+data_set=data/dev_test
+mkdir -p $data_set
+echo $audio_path | rev | cut -d '/' -f 1 | rev | awk -F '.wav' '{print $1}' > $data_set/wav.scp
+cat $data_set/wav.scp | while read lines
+do
+echo $lines $audio_path >> wav2.scp
+done
+mv wav2.scp $data_set/wav.scp
+
+
+cat $data_set/wav.scp | awk -F ' ' '{print $1" "$1}' > $data_set/utt2spk
+#utils/utt2spk_to_spk2utt.pl $data_set/utt2spk > $data_set/spk2utt
+utils/fix_data_dir.sh $data_set
+
+fi
+
+
+#######################################################################
+# Perform feature extraction
+#######################################################################
+if [ $stage -le 2 ]; then
+  # mfccdir should be some place with a largish disk where you
+  # want to store MFCC features.
+  mfccdir=mfcc
+  for x in ${test_sets}; do
+    steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" \
+      --mfcc-config conf/mfcc_hires.conf \
+      data/$x exp/make_mfcc/$x $mfccdir
+  done
+fi
+
+#######################################################################
+# Perform SAD on the dev/eval data
+#######################################################################
+
+dir=exp/segmentation${affix}
+sad_work_dir=exp/sad${affix}_${nnet_type}/
+sad_nnet_dir=$dir/tdnn_${nnet_type}_sad_1a
+if [ $stage -le 3 ]; then
+  for datadir in ${test_sets}; do
+    test_set=data/${datadir}
+    if [ ! -f ${test_set}/wav.scp ]; then
+      echo "$0: Not performing SAD on ${test_set}"
+      exit 0
+    fi
+    ## Perform segmentation
+    local/segmentation/detect_speech_activity.sh --nj $nj --stage $sad_stage \
+     $test_set $sad_nnet_dir mfcc $sad_work_dir \
+     data/${datadir} || exit 1
+
+    test_dir=data/${datadir}_${nnet_type}_seg
+    mv data/${datadir}_seg ${test_dir}/
+   # cp data/${datadir}/{segments,utt2spk} ${test_dir}/              # cp data/${datadir}/{segments.bak,utt2spk.bak} ${test_dir}/
+    # Generate RTTM file from segmentation performed by SAD. This can
+    # be used to evaluate the performance of the SAD as an intermediate
+    # step.
+    steps/segmentation/convert_utt2spk_and_segments_to_rttm.py \
+      ${test_dir}/utt2spk ${test_dir}/segments ${test_dir}/rttm
+  if [ $score == "true" ]; then
+      echo "Scoring $datadir.."
+      # We first generate the reference RTTM from the backed up utt2spk and segments
+      # files.
+      ref_rttm=${test_dir}/ref_rttm
+      steps/segmentation/convert_utt2spk_and_segments_to_rttm.py ${test_dir}/utt2spk.bak \
+        ${test_dir}/segments.bak ${test_dir}/ref_rttm
+
+  fi
+
+ done
+fi
+
+
+
+#######################################################################
+# Perform diarization on the dev/eval data
+#######################################################################
+if [ $stage -le 4 ]; then
+  for datadir in ${test_sets}; do
+    if $use_new_rttm_reference == "true"; then
+      mode="$(cut -d'_' -f1 <<<"$datadir")"
+      ref_rttm=./chime6_rttm/${mode}_rttm
+    else
+      ref_rttm=data/${datadir}_${nnet_type}_seg/ref_rttm
+    fi
+    local/diarize.sh --nj $nj --cmd "$train_cmd" --stage $diarizer_stage \
+      --ref-rttm $ref_rttm \
+      exp/xvector_nnet_1a \
+      data/${datadir}_${nnet_type}_seg \
+      exp/${datadir}_${nnet_type}_seg_diarization
+  done
+fi
+#extract TDOA vectors
+cat data/${test_sets}_seg/segments | while read lines
+do
+start=$(echo $lines | cut -d ' ' -f 3)
+end=$(echo $lines | cut -d ' ' -f 4)
+start_line=$(echo $start/250*1000 | bc -l) 
+end_line=$(echo $end/250*1000 | bc -l) 
+#######################################################################
+# Perform ASR using diarization time stamps
+#######################################################################
+test_dir=${test_sets}_${nnet_type}_seg_asr
+mkdir -p data/$test_dir
+cp data/${test_sets}_${nnet_type}_seg/{segments,wav.scp,spk2utt,utt2spk} data/$test_dir/
+dir=exp/chain_cleaned/tdnn_1d_sp
+graph_dir=$dir/graph_tgsmall
+if [ $stage -le 5 ]; then
+for datadir in ${test_dir}; do
+    steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires_asr.conf \
+      --cmd "$train_cmd" data/${datadir}
+    steps/compute_cmvn_stats.sh data/${datadir}
+    utils/fix_data_dir.sh data/${datadir}
+done
+fi
+
+if [ $stage -le 6 ]; then
+data=$test_dir
+nspk=1
+steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj "${nspk}" \
+      data/${data} exp/nnet3_cleaned/extractor \
+      exp/nnet3_cleaned/ivectors_${data}
+fi
+
+if [ $stage -le 7 ]; then
+utils/mkgraph.sh --self-loop-scale 1.0 --remove-oov \
+  data/lang_test_tgsmall $dir $graph_dir
+fi
+
+if [ $stage -le 8 ]; then
+decode_set=$test_dir
+steps/nnet3/decode.sh --acwt 1.0 --post-decode-acwt 10.0 \
+    --nj ${nj} --cmd "$decode_cmd" \
+    --online-ivector-dir exp/nnet3_cleaned/ivectors_${decode_set} \
+    $graph_dir data/${decode_set} $dir/decode_${decode_set}_tgsmall
+fi
+
+
+#######################################################################
+# Writing Conversation text
+#######################################################################
+mkdir -p $output_dir
+cat $dir/decode_${test_dir}_tgsmall/log/decode* | grep -v "LOG" | grep $input_file > $output_dir/${input_file}_segment
+cp exp/${test_sets}_${nnet_type}_seg_diarization/rttm $output_dir/${input_file}_rttm
+sed -i 's/    / /g' $output_dir/${input_file}_rttm
+sed -i 's/   / /g' $output_dir/${input_file}_rttm
+sed -i 's/  / /g' $output_dir/${input_file}_rttm
+cat data/${test_sets}_${nnet_type}_seg/segments | while read lines
+do
+start=$(echo $lines | cut -d ' ' -f 3)
+spk=$(cat $output_dir/${input_file}_rttm | cut -d ' ' -f 4,8 | grep $start | cut -d ' ' -f 2)
+seg=$(echo $lines | cut -d ' ' -f 1)
+text=$(cat $output_dir/${input_file}_segment | grep $seg | cut -d ' ' -f 2-)
+if [ ! -z "$spk" ]; then
+echo "Speaker "$spk": "$text #>> ${output_dir}/${input_file}_text
+echo "Speaker "$spk": "$text >> ${output_dir}/${input_file}_text
+fi
+done
---
+++ dia/multictext.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+input_file=$1
+output_dir=$2
+
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [options] <input-filename> <out-dir>"
+  echo "If the file has path /home/user/audio/S01audio.CH1.wav, then just give If the file has path /home/user/audio/S01audio as <input-filename> "
+  echo "e.g.:   multictext \\"
+  echo "    S10audio decoded_output/"
+  echo "main options (for others, see top of script file)"
+  echo "  --config <config-file>                # config containing options"
+  echo "  --dereverb <string>                   # Run Single-channel deverberation on multi-channel audio {wpe, nmf }"
+  echo "  --denoise <string>                    # Run Single-channel denoising on multi-channel audio {wiener, spec-sub}"
+  echo "  --localize <string>                   # Type of TDOA estimation for beamforming. {gcc_phat, gcc_scot}"
+  echo "  --beamform <string>                   # Beaforming method {bermformit, dsb, mvdr_ta, mvdr_nn, gev_ta, gev_nn"
+  echo "  --diarize <string>                    # Type of diarization method {tdoa, xvector,xtdoa}"
+  exit 1;
+fi
+
+
+denoise=
+dereverb=
+while getopts dereverb:denoise:localize:beamform:diarize option
+do
+case "${option}"
+in
+dereverb) dereverb=${OPTARG};;
+denoise) denoise=${OPTARG};;
+localize) localize=${OPTARG};;
+beamform) beamform=${OPTARG};;
+diarize) diarize=${OPTARG};;
+esac
+done
+
+num_c=$(ls ${input_file}* | wc -l)
+cp ${input_file}* single_channel/
+input_file=$(echo $input_file | rev | cut -d '/' -f 1 | rev | awk -F '.CH' '{print $1}')
+
+if [ -z "$localize" ]; then
+localize=gcc_phat
+fi
+
+if [ -z "$beamform" ]; then
+beamform=beamformit
+fi
+
+if [ -z "$diarize" ]; then
+diarize=xvector
+fi
+
+
+reverb=1
+noise=1
+
+if [ -z "$dereverb" ] || [ -z "$denoise" ]; then
+
+if [ -z "$denoise" ] && [ ! -z "$dereverb" ] ; then
+#do dereverb only
+reverb=1
+noise=0
+#do wpe
+if [ "$denoise" == "wpe" ]; then
+./codes/dereverb/wpe.py $input_file $num_c
+fi
+#do nmf
+if [ "$denoise" == "nmf" ]; then
+octave -q codes/denoising/enhance.m $input_file NMF $num_c
+fi
+
+elif [ ! -z "$denoise" ] && [ -z "$dereverb" ]; then
+reverb=0
+noise=1
+if [ "$denoise" == "wiener" ]; then
+octave -q codes/denoising/enhance.m $input_file Wiener $num_c
+fi    
+#do denoise only
+if [ "$denoise" == "spec-sub" ]; then
+octave -q codes/denoising/enhance.m $input_file Spec-Sub $num_c
+fi
+
+else
+reverb=0
+noise=0
+fi
+
+fi
+
+
+#Beamforming
+
+mask=$(echo $beamform | cut -d '_' -f 2)
+if [ -z "$dereverb" ]; then 
+dereverb=n
+fi
+if [ -z "$denoise" ]; then
+denoise=n
+fi 
+
+if [ ! -f "${PWD}/out_beamform/${input_file}_${beamform}.wav" ]; then
+
+octave -q codes/enhancement.m $input_file $localize $beamform $noise $reverb $denoise $dereverb ${num_c} $mask
+
+#diarization and ASR
+else
+
+path=$PWD/out_beamform/${input_file}_${beamform}.wav
+./asr_diarize.sh $diarize $path ${input_file}_${beamform} $output_dir 
+fi
+
+
+
