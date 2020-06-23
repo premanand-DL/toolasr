3b79f5ed2d1d92a5ad32c52979ec8c92406f8380
iitbdaplab <34235005+iitbdaplab@users.noreply.github.com>
2020-06-23 17:59:33 +0530

    Update sort_spk.py

--- dia/sort_spk.py
+++ dia/sort_spk.py
@@ -11,14 +11,14 @@ num=1
 for i in range(1,len(lines)):
 	new=0
 	for j in range(len(spk_list)):
-		if int(lines[i].strip()) == spk_list[j]:
+		if int(lines[i].strip().split(' ')[1]) == spk_list[j]:
 			spk.append(j+1)
 			new=1
 			
 	if new==0:
 		num=num+1
 		spk.append(num)
-		spk_list.append(int(lines[i].strip()))
+		spk_list.append(int(lines[i].strip().split(' ')[1]))
 
 
 with open(sys.argv[2],'w') as f:
